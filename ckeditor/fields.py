d89e0446ff50123605aa09c67ac0e9a7be92016e
Eli Dickinson <eli@elidickinson.com>
2011-11-19 18:49:11 -0800

    Add south field definitions for RichTextField

--- ckeditor/fields.py
+++ ckeditor/fields.py
@@ -3,6 +3,10 @@ from django import forms
 
 from ckeditor.widgets import CKEditorWidget
 
+# borrowed from https://github.com/theatlantic/django-ckeditor/commit/0d6f26b6f79b295df8dca1e662644e17ea3c20b5
+from south.modelsinspector import add_introspection_rules
+add_introspection_rules([], ["^ckeditor\.fields\.RichTextField", "^ckeditor\.fields\.RichTextFormField"])
+
 class RichTextField(models.TextField):
     def __init__(self, config_name='default', *args, **kwargs):
         self.config_name = config_name
